fa67db7a5ae7dde7e82323156fe6afe3e229e04d
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2024-10-23 07:47:46 -0600

    agregar menu lateral

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,11 +1,14 @@
-import { Title } from "@/components";
-import { titleFont } from "@/config/fonts";
-import Image from "next/image";
+import { ProductGrid, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+
+const products = initialData.products;
 
 export default function Home() {
 	return (
 		<div className="">
 			<Title title="Tienda" subTitle="Todos lo productos" className="mb-2" />
+
+			<ProductGrid products={products} />
 		</div>
 	);
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,7 @@
 export * from "./ui/not-found/PageNotFound";
-export * from "./ui/top-menu/TopMenu";
+export * from "./ui/sidebar/SideBar";
 export * from "./ui/title/Title";
+export * from "./ui/top-menu/TopMenu";
+
+export * from "./products/product-grid/productGrid";
+export * from "./products/product-grid/ProductGridItem";
