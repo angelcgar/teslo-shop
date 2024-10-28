20417a9ebd129e3361abb109b7dbe0b1f52450e2
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2024-10-28 09:27:54 -0600

    añadir pagina empty

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 
 import { QuantitySelector, Title } from "@/components";
 import { initialData } from "@/seed/seed";
+import { redirect } from "next/navigation";
 
 const productsInCart = [
 	initialData.products[0],
@@ -11,6 +12,8 @@ const productsInCart = [
 ];
 
 export default function () {
+	//todo: redirect('/empty')
+
 	return (
 		<div className="flex justify-center items-center mb-72 px-10 sm:px-0">
 			<div className="flex flex-col w-[1000px]">
