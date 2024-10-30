e2b9af01a1431bf18b632b8c39d01e1ecd733a63
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2024-10-29 21:56:04 -0600

    añadir paginacion manual

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,17 +1,22 @@
 import { getPaginatedProductsWithImages } from "@/actions";
 import { ProductGrid, Title } from "@/components";
-import { initialData } from "@/seed/seed";
 
-// const products = initialData.products;
+interface Props {
+	searchParams: {
+		page?: string;
+	};
+}
 
-export default async function Home() {
-	const { products } = await getPaginatedProductsWithImages();
-	// console.log(products);
+export default async function Home({ searchParams }: Props) {
+	const page = searchParams.page ? Number.parseInt(searchParams.page) : 1;
+
+	const { products } = await getPaginatedProductsWithImages({ page });
 
 	return (
 		<div className="">
 			<Title title="Tienda" subTitle="Todos lo productos" className="mb-2" />
 
+			{/* todo: arreglar este error de tipado */}
 			<ProductGrid products={products} />
 		</div>
 	);
