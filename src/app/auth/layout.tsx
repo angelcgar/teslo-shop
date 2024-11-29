90e2e469cfefa5fae4d3e444d51a68fad4db4827
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2024-11-29 10:52:52 -0600

    resolver conflictos en src/app/auth

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -5,7 +5,7 @@ export default function ShopLayout({
 }) {
 	return (
 		<main className="flex justify-center">
-			<div className="w-full sm:w-[450px] px-10">{children}</div>
+			<div className="w-full sm:w-[350px] px-10">{children}</div>
 		</main>
 	);
 }
--- src/app/auth/new-account/page.tsx
+++ src/app/auth/new-account/page.tsx
@@ -1,18 +1,19 @@
-import { titleFont } from "@/config/fonts";
-import Link from "next/link";
+import { titleFont } from '@/config/fonts';
+import Link from 'next/link';
 
-export default function () {
+export default function NewAccountPage() {
 	return (
 		<div className="flex flex-col min-h-screen pt-32 sm:pt-52">
 			<h1 className={`${titleFont.className} text-4xl mb-5`}>Nueva cuenta</h1>
 
 			<div className="flex flex-col">
-				<label htmlFor="email">Nombre completos</label>
+				<label htmlFor="email">Nombre completo</label>
 				<input
 					className="px-5 py-2 border bg-gray-200 rounded mb-5"
 					type="text"
 				/>
-				<label htmlFor="email">Correo electronico</label>
+
+				<label htmlFor="email">Correo electrónico</label>
 				<input
 					className="px-5 py-2 border bg-gray-200 rounded mb-5"
 					type="email"
